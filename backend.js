017a212c5e793603674c4e264956a5c4dfe81596
Aidan Patrick Marias <52266188+Silver0006@users.noreply.github.com>
2023-11-06 12:55:06 -0500

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,4 +1,11 @@
 console.log("test");
+
+
+
+const firstname = document.createElement('INPUT');
 const newButton = document.createElement('button');
 newButton.textContent = 'Click me!';
+
+
+document.body.appendChild(firstname);
 document.body.appendChild(newButton);
