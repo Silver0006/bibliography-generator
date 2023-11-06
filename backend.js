8cadbbf6f527beeae2d9cfe1e3215ba54ed13f86
Aidan Patrick Marias <52266188+Silver0006@users.noreply.github.com>
2023-11-06 13:19:48 -0500

    Update backend.js

--- backend.js
+++ backend.js
@@ -1,6 +1,6 @@
-document.getElementById('combineButton').addEventListener('click', function() {
-      const string1 = document.getElementById('string1').value;
-      const string2 = document.getElementById('string2').value;
-      const combinedString = string1 + string2;
-      document.getElementById('result').textContent = 'Combined String: ' + combinedString;
+function combineStrings() {
+  const string1 = document.getElementById('string1').value;
+  const string2 = document.getElementById('string2').value;
+  const combinedString = string1 + string2;
+  document.getElementById('result').textContent = 'Combined String: ' + combinedString;
 }
